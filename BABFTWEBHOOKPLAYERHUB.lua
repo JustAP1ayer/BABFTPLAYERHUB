c5e0a4f46a32c19249e764a7f6e84b94b1190740
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-03-09 17:19:51 -0500

    Update BABFTWEBHOOKPLAYERHUB.lua

--- BABFTWEBHOOKPLAYERHUB.lua
+++ BABFTWEBHOOKPLAYERHUB.lua
@@ -1,11 +1,32 @@
 
-local httpService = game:GetService("HttpService")
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+
+local w = library:CreateWindow("PlayerHub BABFT") -- Creates the window
+
+local b = w:CreateFolder("Webhook stuff") -- Creates the folder(U will put here your buttons,etc)
+
+local updatespeed
+b:Slider("Webhook update speed",{
+    min = 30; -- min value of the slider
+    max = 600; -- max value of the slider
+    precise = false; -- max 2 decimals
+},function(value1)
+updatespeed = value1
+end)
+local webhookURL1
+b:Box("Gold Bars Webhook","string",function(value2) -- "number" or "string"
+    webhookURL1 = value2 
+end)
+
+b:Toggle("^ Enable Webhook ^",function(bool3)
+    _G.GoldWebhook = bool3
+    local httpService = game:GetService("HttpService")
 local request = (syn and syn.request) or request or (http and http.request) or http_request
 local LastGoldAmount = game.Players.LocalPlayer.Data.Gold.Value
-while wait(69) do --change this to around 60+, depending on what's right for you
+while _G.GoldWebhook == true do
 local NewGold = game.Players.LocalPlayer.Data.Gold.Value
 request({
-    Url = webhookURL,
+    Url = webhookURL1,
     Method = "POST",
     Headers = {
         ["Content-Type"] = "application/json",
@@ -14,8 +35,8 @@ request({
         content = "",
         embeds = {
             {
-                title = game.Players.LocalPlayer.Name.. "'s Gold Update!",
-                description = "Total Gold Bars Amount: " .. game.Players.LocalPlayer.Data.Gold.Value,
+                title = "|| " ..game.Players.LocalPlayer.Name.. "  || 's Gold Update!",
+                description = "Total Gold Bars Amount:  " .. game.Players.LocalPlayer.Data.Gold.Value,
                 color = 0x81ff57,
                 fields = {
                     {
@@ -30,4 +51,49 @@ request({
     }),
 })
 LastGoldAmount = NewGold 
+wait(updatespeed)
 end
+end)
+
+local WebhookURL2
+b:Box("Gold Blocks Webhook","string",function(value4) -- "number" or "string"
+    WebhookURL2 = value4
+end)
+
+b:Toggle("^ Enable Webhook ^",function(bool5)
+    _G.GoldBarWebhook = bool5
+    local httpService = game:GetService("HttpService")
+local request = (syn and syn.request) or request or (http and http.request) or http_request
+local LastGoldBlockAmount = game:GetService("Players").LocalPlayer.Data.GoldBlock.Value
+while _G.GoldBarWebhook == true do 
+local NewGoldBlock = game:GetService("Players").LocalPlayer.Data.GoldBlock.Value
+request({
+    Url = WebhookURL2,
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json",
+    },
+    Body = httpService:JSONEncode({
+        content = "",
+        embeds = {
+            {
+                title = "|| " ..game.Players.LocalPlayer.Name.. "  || 's Gold Blocks Update!",
+                description = "Total Gold Blocks Amount: " .. game:GetService("Players").LocalPlayer.Data.GoldBlock.Value,
+                color = 0x81ff57,
+                fields = {
+                    {
+                        
+ name = "Updated amount made",
+                        value = "Made " .. tostring(NewGold-LastGoldAmount) ..  " Gold Blocks!", 
+                        inline = true,
+                    },
+                },
+            },
+        },
+    }),
+})
+LastGoldBlockAmount = NewGoldBlock
+wait(updatespeed)
+end
+end)
+

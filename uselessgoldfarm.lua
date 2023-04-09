c9b0146c85634ab0c4d543226b4cddada9d9295d
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-04-09 14:50:22 -0400

    Create uselessgoldfarm.lua

---
+++ uselessgoldfarm.lua
@@ -0,0 +1,51 @@
+local catboyclub = false
+
+function CatboyClub()
+    local player = game.Players.LocalPlayer
+    local rootPart = player.Character:WaitForChild("HumanoidRootPart")
+    rootPart.CFrame = CFrame.new(-48.1823959, 70.0000458, 700)
+    
+    game.Players.LocalPlayer.Character:FindFirstChild("Humanoid").WalkSpeed = 0
+    player.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Physics)
+    
+    local tweenService = game:GetService("TweenService")
+    local part = game.Workspace.BoatStages.NormalStages.CaveStage10.DarknessPart
+    local tweenInfo = TweenInfo.new(
+        21, -- Duration of the tween
+        Enum.EasingStyle.Linear
+    )
+    local goal = {
+        CFrame = part.CFrame
+    }
+    local tween = tweenService:Create(
+        rootPart,
+        tweenInfo,
+        goal
+    )
+    
+    tween:Play()
+    
+    wait(16.5)
+      
+    game.Players.LocalPlayer.Character.Humanoid:Destroy()
+    game.Workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character
+end
+            
+if catboyclub then
+    CatboyClub()
+end
+            
+game.Players.LocalPlayer.CharacterAdded:Connect(function()
+    repeat wait() until game.Players.LocalPlayer.Character
+    wait(2.2)
+    if catboyclub then
+        CatboyClub()
+    end
+end)
+
+    if not catboyclub then
+        if game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") and game:GetService("Players").LocalPlayer.Character then
+        game.Players.LocalPlayer.Character.Humanoid:Destroy()
+            game.Workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character
+        end
+    end
